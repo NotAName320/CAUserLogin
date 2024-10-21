c03b05f7480eb57220499dae43d2b8f82d296d89
Ray Chen <rc144@outlook.com>
2024-10-20 23:57:15 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user of the program.
+     * @param name the username to set
+     */
+    void setCurrentUser(String name);
 }
